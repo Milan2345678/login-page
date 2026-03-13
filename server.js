a5a383590bdb75f1a05ad7ee89d9d14b84ef9598
Milan2345678 <milan.22scse1012665@galgotiasuniversity.edu.in>
2026-03-13 15:41:10 +0530

    remove node_modules from tracking

--- server.js
+++ server.js
@@ -1,32 +1,64 @@
+require("dotenv").config();
+
 const express = require('express');
-const User = require('./models/user');
 const mongoose = require('mongoose');
+const User = require('./models/user');
 
 const app = express();
 
+// MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/loginDB")
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log("MongoDB connection error:", err));
 
-
-
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files
 app.use(express.static('public'));
 
+
+// REGISTER ROUTE
 app.post('/register', async (req, res) => {
-  console.log("Received registration data:", req.body);
+
+  try {
+
+    console.log("Received registration data:", req.body);
+
     const { username, password } = req.body;
-    
-    const newUser = new User({ username, password });
+
+    const newUser = new User({
+      username,
+      password
+    });
+
     await newUser.save();
-res.send(`
-<script>
-alert("User registered successfully");
-window.location.href="/";
-</script>
-`);}); 
+
+    res.send(`
+      <script>
+        alert("User registered successfully!");
+        window.location.href="/";
+      </script>
+    `);
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.send(`
+      <script>
+        alert("Error registering user");
+        window.location.href="/";
+      </script>
+    `);
+
+  }
+
+});
+
+
+// START SERVER
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
